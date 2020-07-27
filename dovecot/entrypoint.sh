a1a50e33bf2fe815b1d40caa0413e0fdbcb54987
Ralf Lang <lang@b1-systems.de>
2020-07-27 18:23:27 +0200

    Fix format of sql connection file

--- dovecot/entrypoint.sh
+++ dovecot/entrypoint.sh
@@ -5,7 +5,7 @@ if [[ -v EXPAND_CONFIGS ]]; then
     echo "EXPANDING CONFIG VARIABLES"
     if [[ -v MYSQL_DATABASE ]]; then
         echo 
-        sed -i "s/connect = .*/connect = 'host=$MYSQL_HOSTNAME dbname=$MYSQL_DATABASE user=$MYSQL_USER password=$MYSQL_PASSWORD';/g" /etc/dovecot/dovecot-sql.conf.ext
+        sed -i "s/connect = .*/connect = host=$MYSQL_HOSTNAME dbname=$MYSQL_DATABASE user=$MYSQL_USER password=$MYSQL_PASSWORD/g" /etc/dovecot/dovecot-sql.conf.ext
     fi
 fi
 exec "$@"
