7e3d1c2b7f6c0b09b5608e593d3543ee53b442d9
Ralf Lang <lang@b1-systems.de>
2020-07-27 09:27:07 +0200

    Move configuration steps to entrypoint
    - put composer bindir into path
    - implement waiting for database. TODO: This is unconditional, we may
      want to suppress this in some scenarios
    - Fix EXPAND_CONFIGS variable name and syntax of created files

--- base/entrypoint.sh
+++ base/entrypoint.sh
@@ -19,37 +19,47 @@ do
 done
 
 ## ADD composer bin_dir to PATH
-export PATH=$PATH:/srv/www/horde/vendor/bin
+echo "export PATH=\$PATH:/srv/www/horde/vendor/bin" > /root/.bashrc
 
 ## Replace some well-known template variables in the horde config
-if [[ -v EXPAND_CONFIG ]]; then
+if [[ -v EXPAND_CONFIGS ]]; then
     echo "EXPANDING CONFIG VARIABLES"
     if [[ -v MYSQL_DATABASE ]]; then
         echo 
-        sed -i "s/conf\['sql'\]\['database'\].*/conf['sql']['database'] = '$MYSQL_DATABASE'/g" /srv/www/horde/web/horde/config/conf.php
+        sed -i "s/conf\['sql'\]\['database'\].*/conf['sql']['database'] = '$MYSQL_DATABASE';/g" /srv/www/horde/web/horde/config/conf.php
     fi
     if [[ -v MYSQL_HOSTNAME ]]; then
         echo 
-        sed -i "s/conf\['sql'\]\['hostspec'\].*/conf['sql']['hostspec'] = '$MYSQL_HOSTNAME'/g" /srv/www/horde/web/horde/config/conf.php
+        sed -i "s/conf\['sql'\]\['hostspec'\].*/conf['sql']['hostspec'] = '$MYSQL_HOSTNAME';/g" /srv/www/horde/web/horde/config/conf.php
     fi
     if [[ -v MYSQL_USER ]]; then
         echo 
-        sed -i "s/conf\['sql'\]\['username'\].*/conf['sql']['username'] = '$MYSQL_USER'/g" /srv/www/horde/web/horde/config/conf.php
+        sed -i "s/conf\['sql'\]\['username'\].*/conf['sql']['username'] = '$MYSQL_USER';/g" /srv/www/horde/web/horde/config/conf.php
     fi
     if [[ -v MYSQL_PASSWORD ]]; then
         echo 
-        sed -i "s/conf\['sql'\]\['password'\].*/conf['sql']['password'] = '$MYSQL_PASSWORD'/g" /srv/www/horde/web/horde/config/conf.php
+        sed -i "s/conf\['sql'\]\['password'\].*/conf['sql']['password'] = '$MYSQL_PASSWORD';/g" /srv/www/horde/web/horde/config/conf.php
     fi
 fi
 
-## TODO: Wait for DB connection to succeed
 ## TODO: BACKGROUND THIS and everything besides making apache pid one
+## Wait for DB connection to succeed
+echo "SHOW DATABASES" > /root/conntest.sql
+echo "WAITING FOR DB CONNECTION TO SUCCEED"
+echo "For new setups, this may take some time. You may see error messages"
+until php /srv/www/horde/web/horde/bin/horde-sql-shell /root/conntest.sql &> /dev/null
+do
+    sleep 3
+    echo "RETRYING"
+done
+echo "CONNECTION ESTABLISHED"
 
-## TODO: Run migrations N times
+## Run migrations N times
 if [[ -v HORDE_MIGRATION_RUNS ]]
 then
     for i in {1..$HORDE_MIGRATION_RUNS}
     do
+        echo "Running Horde Schema Migrations: #$i"
         php /srv/www/horde/web/horde/bin/horde-db-migrate
    done
 fi
@@ -57,7 +67,7 @@ fi
 ## TODO: Inject initial horde user
 if [[ -v HORDE_ADMIN_USER ]]; then
     echo "Injecting Admin User $HORDE_ADMIN_USER"
-    php /srv/www/horde/web/horde/bin/createUser.php $HORDE_ADMIN_USER $HORDE_ADMIN_PASSWORD
+    php /srv/www/horde/web/horde/bin/createUser.php $HORDE_ADMIN_USER $HORDE_ADMIN_PASSWORD &> /dev/null
 fi
 
 echo "Handing over to pid 1 command"
