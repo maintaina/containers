501848989a7fde9e3aa5247ff4668e034e574f1e
Felix Glaser <glaser@b1-systems.de>
2020-09-21 12:26:08 +0200

    postfix: add configuration files and mysql config expansion

--- postfix/entrypoint.sh
+++ postfix/entrypoint.sh
@@ -4,7 +4,14 @@
 if [[ -v EXPAND_CONFIGS ]]; then
     echo "EXPANDING CONFIG VARIABLES"
     if [[ -v MYSQL_DATABASE ]]; then
-        echo "Filling out *maps.cf templates (TODO)"
+        echo "Filling out *maps.cf templates"
+        sed -i -e "s/user = postfix/user = $MYSQL_USER/" \
+               -e "s/password = postfix/password = $MYSQL_PASSWORD/" \
+               -e "s/hosts = localhost/hosts = $MYSQL_HOSTNAME/" \
+               -e "s/dbname = postfix/dbname = $MYSQL_DATABASE/" \
+            /etc/postfix/mysql_virtual_alias_maps.cf \
+            /etc/postfix/mysql_virtual_domains_maps.cf \
+            /etc/postfix/mysql_virtual_mailbox_maps.cf
     fi
 fi
 exec "$@"
