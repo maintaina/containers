9a403b700f89354489ca6b251eb4189e0af68dd7
Felix Glaser <glaser@b1-systems.de>
2020-09-21 16:59:43 +0200

    postfix: modify entrypoint.sh to configure mysql backend for postfix

--- postfix/entrypoint.sh
+++ postfix/entrypoint.sh
@@ -4,14 +4,36 @@
 if [[ -v EXPAND_CONFIGS ]]; then
     echo "EXPANDING CONFIG VARIABLES"
     if [[ -v MYSQL_DATABASE ]]; then
-        echo "Filling out *maps.cf templates"
-        sed -i -e "s/user = postfix/user = $MYSQL_USER/" \
-               -e "s/password = postfix/password = $MYSQL_PASSWORD/" \
-               -e "s/hosts = localhost/hosts = $MYSQL_HOSTNAME/" \
-               -e "s/dbname = postfix/dbname = $MYSQL_DATABASE/" \
-            /etc/postfix/mysql_virtual_alias_maps.cf \
-            /etc/postfix/mysql_virtual_domains_maps.cf \
+        # fix permissions of volume-provided configs
+        chown root:postfix /etc/postfix/mysql*
+
+        # NOTE: sed -i does not work here and fails with "Device or resource busy..."
+
+        # fill in the virtual_mailbox_domains config
+        sed -e "s/user = postfix/user = $MYSQL_USER/" \
+            -e "s/password = postfix/password = $MYSQL_PASSWORD/" \
+            -e "s/hosts = localhost/hosts = $MYSQL_HOSTNAME/" \
+            -e "s/dbname = postfix/dbname = $MYSQL_DATABASE/" \
+            /etc/postfix/mysql_virtual_mailbox_domains.cf > \
+            /etc/postfix/mysql_virtual_mailbox_domains.cf.new
+        cp  /etc/postfix/mysql_virtual_mailbox_domains.cf.new \
+            /etc/postfix/mysql_virtual_mailbox_domains.cf
+        rm /etc/postfix/mysql_virtual_mailbox_domains.cf.new
+
+        # fill in the virtual_mailbox_maps config
+        sed -e "s/user = postfix/user = $MYSQL_USER/" \
+            -e "s/password = postfix/password = $MYSQL_PASSWORD/" \
+            -e "s/hosts = localhost/hosts = $MYSQL_HOSTNAME/" \
+            -e "s/dbname = postfix/dbname = $MYSQL_DATABASE/" \
+            /etc/postfix/mysql_virtual_mailbox_maps.cf > \
+            /etc/postfix/mysql_virtual_mailbox_maps.cf.new
+        cp /etc/postfix/mysql_virtual_mailbox_maps.cf.new \
             /etc/postfix/mysql_virtual_mailbox_maps.cf
+        rm /etc/postfix/mysql_virtual_mailbox_maps.cf.new
+
+        # add the virtual_mailbox_* configs to the main.cf
+        postconf virtual_mailbox_domains=mysql:/etc/postfix/mysql_virtual_mailbox_domains.cf
+        postconf virtual_mailbox_maps=mysql:/etc/postfix/mysql_virtual_mailbox_maps.cf
     fi
 fi
 exec "$@"
