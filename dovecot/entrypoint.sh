e4659e2eacef4ca85c67ccbd334874898929c551
Marc Unger <unger@b1-systems.de>
2020-09-23 14:23:51 +0200

    Dovecot config (submission port, sasl, sql query, entrypoint sed adjustments).

--- dovecot/entrypoint.sh
+++ dovecot/entrypoint.sh
@@ -4,8 +4,10 @@
 if [[ -v EXPAND_CONFIGS ]]; then
     echo "EXPANDING CONFIG VARIABLES"
     if [[ -v MYSQL_DATABASE ]]; then
-        echo 
-        sed -i "s/connect = .*/connect = host=$MYSQL_HOSTNAME dbname=$MYSQL_DATABASE user=$MYSQL_USER password=$MYSQL_PASSWORD/g" /etc/dovecot/dovecot-sql.conf.ext
+        echo "Setting env vars in /etc/dovecot/dovecot-sql.conf.ext"
+        sed "s/connect = .*/connect = host=$MYSQL_HOSTNAME dbname=$MYSQL_DATABASE user=$MYSQL_USER password=$MYSQL_PASSWORD/g" /etc/dovecot/dovecot-sql.conf.ext > /etc/dovecot/tmp-sql.conf
+	cp /etc/dovecot/tmp-sql.conf /etc/dovecot/dovecot-sql.conf.ext
+	rm /etc/dovecot/tmp-sql.conf
     fi
 fi
 exec "$@"
