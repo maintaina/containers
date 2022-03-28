11da0143f8f10b75e703570003ebb648075ea594
Felix Glaser <glaser@b1-systems.de>
2022-03-28 16:32:01 +0200

    configure xdebug in entrypoint, if in developer mode

--- base/bin/entrypoint.sh
+++ base/bin/entrypoint.sh
@@ -4,6 +4,7 @@ echo "Config stage"
 ## copy files from /srv/original_config/apps to config dirs
 /usr/local/bin/copy-original-configs
 
+# TODO: remove this section, docker-compose >= 1.26 supports quotes in .env
 ## FIXUP VARIABLES IF THEY CONTAIN LEADING/TRAILING QUOTES
 
 tmp="${GITHUB_COMPOSER_TOKEN%\"}"
@@ -21,9 +22,9 @@ export HORDE_ADMIN_PASSWORD=$tmp
 ## ADD composer bin_dir to PATH
 echo "export PATH=\$PATH:/srv/www/horde/vendor/bin" > /root/.bashrc
 
-if [[ ! -z $GITHUB_COMPOSER_TOKEN ]]; then
+if [[ -n $GITHUB_COMPOSER_TOKEN ]]; then
 	echo "Configuring authentication to Github API for composer"
-	composer --no-interaction config -g github-oauth.github.com $GITHUB_COMPOSER_TOKEN
+	composer --no-interaction config -g github-oauth.github.com "$GITHUB_COMPOSER_TOKEN"
 fi
 
 ## Replace some well-known template variables in the horde config
@@ -57,7 +58,7 @@ fi
 ## TODO: BACKGROUND THIS and everything besides making the main process pid 1
 ## Wait for DB connection to succeed
 ## TODO: Make this optional for No-DB scenarios
-if [[ ! -z $MYSQL_PASSWORD ]]; then
+if [[ -n $MYSQL_PASSWORD ]]; then
 	echo "SHOW DATABASES" >/root/conntest.sql
 	echo "WAITING FOR DB CONNECTION TO SUCCEED"
 	echo "For new setups, this may take some time. You may see error messages"
@@ -82,7 +83,7 @@ fi
 # Inject initial user or change his password if he already exists
 if [[ -v HORDE_ADMIN_USER ]]; then
 	echo "Injecting Admin User $HORDE_ADMIN_USER"
-	php /srv/www/horde/vendor/bin/hordectl patch user $HORDE_ADMIN_USER $HORDE_ADMIN_PASSWORD
+	php /srv/www/horde/vendor/bin/hordectl patch user "$HORDE_ADMIN_USER" "$HORDE_ADMIN_PASSWORD"
 fi
 
 # if ENABLE_DEVELOPER_MODE=yes then install the developer tools marked with yes (e.g. VIM=yes)
@@ -91,12 +92,30 @@ if [[ -v ENABLE_DEVELOPER_MODE && -n "$ENABLE_DEVELOPER_MODE" && $ENABLE_DEVELOP
 	PHP_VERSION=$(php -r "echo PHP_VERSION;")
 
 	if [[ $PHP_VERSION == 7.* ]]; then
-		zypper -n in php7-xdebug
+		zypper -n install php7-xdebug
 	fi
 	if [[ $PHP_VERSION == 8.* ]]; then
-		zypper -n in php8-xdebug
+		zypper -n install php8-xdebug
 	fi
-	zypper -n in vim wget mc iputils curl less bind-utils 	
+
+    xdebug_major_version="$(rpm -qa '*xdebug*' --qf "%{VERSION}\n" | cut -d'.' -f1)"
+    if [[ $xdebug_major_version -eq 2 ]]; then
+        # configuration for Xdebug 2.9.5 as distributed by openSUSE Leap 15.3
+        {
+            echo "xdebug.remote_enable = 1";
+            echo "xdebug.remote_autostart = 1";
+            echo "xdebug.remote_port = 9000";
+        } >> /etc/php7/conf.d/xdebug.ini
+    elif [[ $xdebug_major_version -eq 3 ]]; then
+        # NOTE: untested as of 2022-03-04, as only Tumbleweed has Xdebug 3
+        {
+            echo "xdebug.mode = develop,debug";
+            echo "xdebug.start_with_request = yes";
+        } >> /etc/php7/conf.d/xdebug.ini
+    fi
+    unset xdebug_major_version
+
+	zypper -n install vim vim-data wget mc iputils curl less bind-utils
 
 	echo "ending installation of developer-tools"
 else
@@ -106,7 +125,7 @@ fi
 # if CUSTOM_TOOLS is not empty try to install them
 if [[ -v CUSTOM_TOOLS && -n "$CUSTOM_TOOLS" ]]; then
 	echo "Custom tools to be installed: $CUSTOM_TOOLS"
-	zypper -n in $CUSTOM_TOOLS
+	zypper -n install "$CUSTOM_TOOLS"
 fi
 
 echo "Handing over to pid 1 command"
